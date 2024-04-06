66978dc979493c199372ff6b5f5ca778bf62f3ab
GantaVenkatakousik <venkatakousikcse01@gmail.com>
2024-04-07 00:08:29 +0530

    commiting all the changes

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
 import Tags from './components/Tags'
 import SearchProfile from './components/SearchProfile'
 
 function Nav () {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
   return (
-    <nav className='nav container'>
-      <ul className='elements'>
+    <>
+      <nav className='nav container'>
+        <ul className='elements'>
+          <div className='logo'>
+            <i class='fa-solid fa-dna'></i>
+          </div>
+          <Tags />
+          <SearchProfile />
+        </ul>
+      </nav>
+      <nav className='nav-responsive '>
         <div className='logo'>
-          <i class='fa-solid fa-dna'></i>
+          <i className='fa-solid fa-dna'></i>
         </div>
-        <Tags />
-        <SearchProfile />
-      </ul>
-    </nav>
+        <div
+          className={`menu-toggle ${showMenu ? 'active' : ''}`}
+          onClick={toggleMenu}
+        >
+          <i className={`fa-solid ${showMenu ? 'fa-xmark' : 'fa-bars'}`}></i>
+        </div>
+        {showMenu && (
+          <ul className='responsive-menu'>
+            <Tags />
+            <li>
+              <i className='fa-solid fa-user'></i>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </>
   )
 }
 
