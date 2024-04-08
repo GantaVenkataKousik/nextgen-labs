25585eea793fd0252e32125e5fb0f7f143c94b66
GantaVenkatakousik <venkatakousikcse01@gmail.com>
2024-04-08 20:00:16 +0530

    commiting all the changes

---
+++ client/src/pages/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+import './responsive.css'
+
+function Footer () {
+  return (
+    <>
+      <div className='Footer'></div>
+    </>
+  )
+}
+
+export default Footer
--- client/src/pages/Founder/Founder.jsx
+++ client/src/pages/Founder/Founder.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './style.css'
-import './responsive.css'
 
 function Founder () {
   return (
@@ -8,11 +7,31 @@ function Founder () {
       <div className='Founder'>
         <h1>Founder & CEO of this Labs</h1>
 
-        <h3>
+        <h3 className='tag'>
           Our Emperor K.M. Sridevi always thinks of serving the best to the
           people <br></br> her guidance and supervision is the main reason
           behind all this
         </h3>
+        <div className='founderDetails'>
+          <div></div>
+          <div className='founder'>
+            <img src='./assets/images/founder/sridevi.png'></img>
+            <div className='info'>
+              <h2>K.M. Sridevi</h2>
+              <h2>Founder & CEO</h2>
+              <h3>Expert in Neuro Science</h3>
+            </div>
+          </div>
+          <div></div>
+          <div className='motive'>
+            <div className='content'>
+              <p>
+                My principle is simple I don't want profits , what I want is
+                trust and support from you.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
