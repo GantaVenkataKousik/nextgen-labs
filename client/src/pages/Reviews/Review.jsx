58657c1c1d2ad38ba398d2193bd1cc85f51aa3e7
gvk <venkatakousikcse01@gmail.com>
2024-07-28 23:33:45 +0530

    style(ui):added styling to the reviews folder firsting placing the user icon and then the name and designation and then the review

--- client/src/pages/Reviews/Review.jsx
+++ client/src/pages/Reviews/Review.jsx
@@ -1,33 +1,24 @@
-import React from 'react'
-import './style.css'
-import './responsive.css'
+import React from 'react';
+import './responsive.css';
 
-function Review() {
+function Review({ review }) {
   return (
-    <>
-      <div className='Review'>
-        <div className='dp'>
-          <img src='./assets/images/reviews/m3.jpg'></img>
-          <div className='user'>
-            <h2>G.VenkataKousik</h2>
-            <h3>Full Stack Developer</h3>
-          </div>
-        </div>
-        <div className='rating'>
-          <i className='fa-solid fa-star'></i>
-          <i className='fa-solid fa-star'></i>
-          <i className='fa-solid fa-star'></i>
-          <i className='fa-solid fa-star'></i>
-          <span className='ratingNum'>4.0/5.0</span>
-        </div>
-        <div className='review'>
-          Loved their hospitality , the resulat that I got there was the best in
-          my life. The discounts are very high compared to other restaurants .
-          The order was given in no time , the waiting time is very less .{' '}
-        </div>
+    <div className='Review'>
+      <img src='/person_icon.png' alt={`${review.name}'s review`} />
+      <h2 className='username'>{review.name}</h2>
+      <h3 className='role'>{review.role}</h3>
+      <div className='rating'>
+        {[...Array(Math.floor(review.rating))].map((_, index) => (
+          <i key={index} className='fa-solid fa-star'></i>
+        ))}
+        {review.rating % 1 > 0 && <i className='fa-solid fa-star-half-stroke'></i>}
+        <span className='ratingNum'>{review.rating}/5.0</span>
       </div>
-    </>
-  )
+      <div className='review'>
+        {review.comment}
+      </div>
+    </div>
+  );
 }
 
-export default Review
+export default Review;
